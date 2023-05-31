16a632aa1fbe8e7ed10ab57bdb6f5f04825f63fb
ivanlele <ivanlele402@gmail.com>
2023-05-31 16:45:13 +0000

    add logs for the retry_until_success

--- src/http.rs
+++ src/http.rs
@@ -1,10 +1,16 @@
 #[macro_export]
 macro_rules! retry_until_success {
     ($func:expr) => {{
+        let mut attempts = 1;
         let mut result = $func.await;
         while result.is_err() && format!("{:?}", result.as_ref().unwrap_err()).contains("The http_request resulted into error. RejectionCode: SysTransient, Error: Canister http responses were different across replicas, and no consensus was reached") {
             result = $func.await;
+            attempts += 1;
         }
+
+        let (func_name, _) = stringify!($func).split_once("(").unwrap();
+
+        ic_utils::logger::log_message(format!("[{func_name}] used attempts: {attempts}"));
 
         result
     }}
