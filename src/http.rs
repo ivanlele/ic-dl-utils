2543e3f471ba90cc94dc23f9ab6cc06d32a988ee
ivanlele <ivanlele402@gmail.com>
2023-05-31 14:43:48 +0000

    add tests

--- src/http.rs
+++ src/http.rs
@@ -1,14 +1,27 @@
-#[allow(dead_code)]
-const CONSENCUS_ERROR: &str = "The http_request resulted into error. RejectionCode: SysTransient, Error: Canister http responses were different across replicas, and no consensus was reached";
-
 #[macro_export]
 macro_rules! retry_until_success {
-    ($func:expr, $($arg:expr),*) => {{
-        let mut result = $func($($args),*).await;
-        while result.is_err() && format!("{:?}", result.unwrap_err()).contains(CONSENCUS_ERROR) {
-            result = $func($($args),*).await;
+    ($func:expr) => {{
+        let mut result = $func.await;
+        while result.is_err() && format!("{:?}", result.as_ref().unwrap_err()).contains("The http_request resulted into error. RejectionCode: SysTransient, Error: Canister http responses were different across replicas, and no consensus was reached") {
+            result = $func.await;
         }
 
         result
-    }};
+    }}
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::retry_until_success;
+
+    use anyhow::Result;
+
+    async fn test_func() -> Result<()> {
+        return Ok(())
+    }
+
+    #[tokio::test]
+    async fn it_works() {
+        let _ = retry_until_success!(test_func());
+    }
 }
