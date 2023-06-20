b342566f647b815d1dc453f7e56c7f8a0296979e
ivanlele <ivanlele402@gmail.com>
2023-06-20 21:05:11 +0000

    erase

--- src/http.rs
+++ src/http.rs
@@ -3,10 +3,11 @@ macro_rules! retry_until_success {
     ($func:expr) => {{
         let mut attempts = 1;
         let mut result = $func.await;
+
         while result.is_err() && format!("{:?}", result.as_ref().unwrap_err()).contains("The http_request resulted into error. RejectionCode: SysTransient, Error: Canister http responses were different across replicas, and no consensus was reached") {
             result = $func.await;
             attempts += 1;
-        }
+        }        
 
         let (func_name, _) = stringify!($func).rsplit_once("(").unwrap();
 
