8680465b641f41e1a7054b8df8241ae7035a4530
ivanlele <ivanlele402@gmail.com>
2023-06-23 11:41:42 +0000

    add logs

--- src/http.rs
+++ src/http.rs
@@ -4,16 +4,18 @@ macro_rules! retry_until_success {
         let mut attempts = 1;
         let mut result = $func.await;
 
+        let (func_name, _) = stringify!($func).rsplit_once("(").unwrap();
+
         while result.is_err() 
             && (format!("{:?}", result.as_ref().unwrap_err()).contains("The http_request resulted into error. RejectionCode: SysTransient, Error: Canister http responses were different across replicas, and no consensus was reached")
             || format!("{:?}", result.as_ref().unwrap_err()).contains("insufficient funds for gas * price + value")) {
             result = $func.await;
+            ic_cdk::println!("[{func_name}] use attempts: {attempts}");
             attempts += 1;
         }        
 
-        let (func_name, _) = stringify!($func).rsplit_once("(").unwrap();
-
         ic_utils::logger::log_message(format!("[{func_name}] used attempts: {attempts}"));
+        ic_cdk::println!("[{func_name}] used attempts: {attempts}");
 
         result
     }}
